8bfe6526b3dc43406d48bb0f371011dc4be8b853
Joao Matos <joao@tritao.eu>
2022-02-22 13:28:29 -0300

    Refactor country picker build method as statement.

--- lib/country_picker_dialog.dart
+++ lib/country_picker_dialog.dart
@@ -69,73 +69,74 @@ class _CountryPickerDialogState extends State<CountryPickerDialog> {
   }
 
   @override
-  Widget build(BuildContext context) => Dialog(
-        backgroundColor: widget.style?.backgroundColor,
-        child: Container(
-          padding: widget.style?.padding ?? EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: widget.style?.searchFieldPadding ?? EdgeInsets.all(0),
-                child: TextField(
-                  cursorColor: widget.style?.searchFieldCursorColor,
-                  decoration: widget.style?.searchFieldInputDecoration ??
-                      InputDecoration(
-                        suffixIcon: Icon(Icons.search),
-                        labelText: widget.searchText,
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: widget.style?.backgroundColor,
+      child: Container(
+        padding: widget.style?.padding ?? EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: widget.style?.searchFieldPadding ?? EdgeInsets.all(0),
+              child: TextField(
+                cursorColor: widget.style?.searchFieldCursorColor,
+                decoration: widget.style?.searchFieldInputDecoration ??
+                    InputDecoration(
+                      suffixIcon: Icon(Icons.search),
+                      labelText: widget.searchText,
+                    ),
+                onChanged: (value) {
+                  _filteredCountries = isNumeric(value)
+                      ? widget.countryList
+                          .where((country) => country.dialCode.contains(value))
+                          .toList()
+                      : widget.countryList
+                          .where((country) => country.name
+                              .toLowerCase()
+                              .contains(value.toLowerCase()))
+                          .toList();
+                  if (this.mounted) setState(() {});
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _filteredCountries.length,
+                itemBuilder: (ctx, index) => Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Image.asset(
+                        'assets/flags/${_filteredCountries[index].code.toLowerCase()}.png',
+                        package: 'intl_phone_field',
+                        width: 32,
                       ),
-                  onChanged: (value) {
-                    _filteredCountries = isNumeric(value)
-                        ? widget.countryList
-                            .where(
-                                (country) => country.dialCode.contains(value))
-                            .toList()
-                        : widget.countryList
-                            .where((country) => country.name
-                                .toLowerCase()
-                                .contains(value.toLowerCase()))
-                            .toList();
-                    if (this.mounted) setState(() {});
-                  },
+                      contentPadding: widget.style?.listTilePadding,
+                      title: Text(
+                        _filteredCountries[index].name,
+                        style: widget.style?.countryNameStyle ??
+                            TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      trailing: Text(
+                        '+${_filteredCountries[index].dialCode}',
+                        style: widget.style?.countryCodeStyle ??
+                            TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      onTap: () {
+                        _selectedCountry = _filteredCountries[index];
+                        widget.onCountryChanged(_selectedCountry);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    widget.style?.listTileDivider ?? Divider(thickness: 1),
+                  ],
                 ),
               ),
-              SizedBox(height: 20),
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: _filteredCountries.length,
-                  itemBuilder: (ctx, index) => Column(
-                    children: <Widget>[
-                      ListTile(
-                        leading: Image.asset(
-                          'assets/flags/${_filteredCountries[index].code.toLowerCase()}.png',
-                          package: 'intl_phone_field',
-                          width: 32,
-                        ),
-                        contentPadding: widget.style?.listTilePadding,
-                        title: Text(
-                          _filteredCountries[index].name,
-                          style: widget.style?.countryNameStyle ??
-                              TextStyle(fontWeight: FontWeight.w700),
-                        ),
-                        trailing: Text(
-                          '+${_filteredCountries[index].dialCode}',
-                          style: widget.style?.countryCodeStyle ??
-                              TextStyle(fontWeight: FontWeight.w700),
-                        ),
-                        onTap: () {
-                          _selectedCountry = _filteredCountries[index];
-                          widget.onCountryChanged(_selectedCountry);
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      widget.style?.listTileDivider ?? Divider(thickness: 1),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
