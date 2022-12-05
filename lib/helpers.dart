1d743ef2dbf20fe6a4b60ca30ead1803f4f8fd43
Launay Guillaume <g.launay96@gmail.com>
2022-12-05 21:43:20 +0100

    improve search

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -3,14 +3,29 @@ import 'package:intl_phone_field/countries.dart';
 bool isNumeric(String s) =>
     s.isNotEmpty && int.tryParse(s.replaceAll("+", "")) != null;
 
+String removeDiacritics(String str) {
+  var withDia =
+      'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+  var withoutDia =
+      'AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz';
+
+  for (int i = 0; i < withDia.length; i++) {
+    str = str.replaceAll(withDia[i], withoutDia[i]);
+  }
+
+  return str;
+}
+
 extension CountryExtensions on List<Country> {
   List<Country> stringSearch(String search) {
+    search = removeDiacritics(search.toLowerCase());
     return where(
       (country) => isNumeric(search) || search.startsWith("+")
-          ? country.dialCode.contains(search.replaceAll("+", ""))
-          : country.name.toLowerCase().contains(search.toLowerCase()) ||
-              country.nameTranslations.values
-                  .any((element) => element.contains(search)),
+          ? country.dialCode.contains(search)
+          : removeDiacritics(country.name.replaceAll("+", "").toLowerCase())
+                  .contains(search) ||
+              country.nameTranslations.values.any((element) =>
+                  removeDiacritics(element.toLowerCase()).contains(search)),
     ).toList();
   }
 }
