0f7df3e093c698f0f34ca02a99fd025696b5f1d3
haidarmehsen <125974245+haidarmehsen@users.noreply.github.com>
2023-04-13 13:32:00 +0300

    formatted code

--- lib/phone_number.dart
+++ lib/phone_number.dart
@@ -1,8 +1,10 @@
 import 'countries.dart';
 
-class NumberTooLongException implements Exception{}
-class NumberTooShortException implements Exception{}
-class InvalidCharactersException implements Exception{}
+class NumberTooLongException implements Exception {}
+
+class NumberTooShortException implements Exception {}
+
+class InvalidCharactersException implements Exception {}
 
 class PhoneNumber {
   String countryISOCode;
@@ -15,44 +17,42 @@ class PhoneNumber {
     required this.number,
   });
 
-  factory PhoneNumber.fromCompleteNumber({required String completeNumber}){
-    if(completeNumber == "") {
-      return PhoneNumber(countryISOCode: "",
-          countryCode: "",
-          number: "");
+  factory PhoneNumber.fromCompleteNumber({required String completeNumber}) {
+    if (completeNumber == "") {
+      return PhoneNumber(countryISOCode: "", countryCode: "", number: "");
     }
 
-    try{
+    try {
       Country country = getCountry(completeNumber);
       String number;
       if (completeNumber.startsWith('+')) {
-        number = completeNumber.substring(1+country.dialCode.length+country.regionCode.length);
+        number = completeNumber
+            .substring(1 + country.dialCode.length + country.regionCode.length);
       } else {
-        number = completeNumber.substring(country.dialCode.length+country.regionCode.length);
+        number = completeNumber
+            .substring(country.dialCode.length + country.regionCode.length);
       }
-      return PhoneNumber(countryISOCode: country.code,
+      return PhoneNumber(
+          countryISOCode: country.code,
           countryCode: country.dialCode + country.regionCode,
           number: number);
-    } on InvalidCharactersException{
+    } on InvalidCharactersException {
       rethrow;
-    } on Exception catch(e){
-      return PhoneNumber(countryISOCode: "",
-          countryCode: "",
-          number: "");
+    } on Exception catch (e) {
+      return PhoneNumber(countryISOCode: "", countryCode: "", number: "");
     }
-
   }
 
-  bool isValidNumber(){
-      Country country = getCountry(completeNumber);
-      if( number.length < country.minLength){
-        throw NumberTooShortException();
-      }
+  bool isValidNumber() {
+    Country country = getCountry(completeNumber);
+    if (number.length < country.minLength) {
+      throw NumberTooShortException();
+    }
 
-      if( number.length > country.maxLength){
-        throw NumberTooLongException();
-      }
-      return true;
+    if (number.length > country.maxLength) {
+      throw NumberTooLongException();
+    }
+    return true;
   }
 
   String get completeNumber {
@@ -60,21 +60,20 @@ class PhoneNumber {
   }
 
   static Country getCountry(String phoneNumber) {
-    if(phoneNumber == ""){
+    if (phoneNumber == "") {
       throw NumberTooShortException();
     }
 
     final _validPhoneNumber = RegExp(r'^[+0-9]*[0-9]*$');
 
-    if(!_validPhoneNumber.hasMatch(phoneNumber)){
+    if (!_validPhoneNumber.hasMatch(phoneNumber)) {
       throw InvalidCharactersException();
     }
 
     if (phoneNumber.startsWith('+')) {
-      return countries.firstWhere((country) =>
-          phoneNumber
-              .substring(1)
-              .startsWith(country.dialCode + country.regionCode));
+      return countries.firstWhere((country) => phoneNumber
+          .substring(1)
+          .startsWith(country.dialCode + country.regionCode));
     }
     return countries.firstWhere((country) =>
         phoneNumber.startsWith(country.dialCode + country.regionCode));
--- tests/intl_phone_field_test.dart
+++ tests/intl_phone_field_test.dart
@@ -3,7 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 
 class TestWidget extends StatelessWidget {
-  const TestWidget({Key? key, required this.phoneNumber, this.countryCode}) : super(key: key);
+  const TestWidget({Key? key, required this.phoneNumber, this.countryCode})
+      : super(key: key);
 
   final String phoneNumber;
   final String? countryCode;
@@ -37,30 +38,30 @@ void main() {
   });
 
   testWidgets('Test intl_phone_field setup with Guernsey number: +441481960194',
-          (WidgetTester tester) async {
-        await tester.pumpWidget(TestWidget(
-          phoneNumber: '+441481960194',
-          countryCode: 'GG',
-        ));
+      (WidgetTester tester) async {
+    await tester.pumpWidget(TestWidget(
+      phoneNumber: '+441481960194',
+      countryCode: 'GG',
+    ));
 
-        final countryCodeFinder = find.text('+44 1481');
-        final numberFinder = find.text('960194');
+    final countryCodeFinder = find.text('+44 1481');
+    final numberFinder = find.text('960194');
 
-        expect(countryCodeFinder, findsOneWidget);
-        expect(numberFinder, findsOneWidget);
-      });
+    expect(countryCodeFinder, findsOneWidget);
+    expect(numberFinder, findsOneWidget);
+  });
 
   testWidgets('Test intl_phone_field setup with UK number: +447891244567',
-          (WidgetTester tester) async {
-        await tester.pumpWidget(TestWidget(
-          phoneNumber: '+447891244567',
-          countryCode: 'GB',
-        ));
+      (WidgetTester tester) async {
+    await tester.pumpWidget(TestWidget(
+      phoneNumber: '+447891244567',
+      countryCode: 'GB',
+    ));
 
-        final countryCodeFinder = find.text('+44');
-        final numberFinder = find.text('7891244567');
+    final countryCodeFinder = find.text('+44');
+    final numberFinder = find.text('7891244567');
 
-        expect(countryCodeFinder, findsOneWidget);
-        expect(numberFinder, findsOneWidget);
-      });
+    expect(countryCodeFinder, findsOneWidget);
+    expect(numberFinder, findsOneWidget);
+  });
 }
